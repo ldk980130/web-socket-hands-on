dbae7ef96958787a5569bb32e2f01d82b96f1715
does <ldk980130@gmail.com>
2023-11-01 20:09:02 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/study/websockethandson/WebSocketHandsOnApplication.java
@@ -0,0 +1,13 @@
+package com.study.websockethandson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketHandsOnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketHandsOnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/websockethandson/WebSocketHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.websockethandson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketHandsOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
